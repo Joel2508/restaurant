8940a55f1ef0436a2218e28776b6ce5eb1228217
Joel2508 <Joelarias2508@gmail.com>
2021-02-16 11:49:39 -0400

    Add icon in the proyect restaurant

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -1,6 +1,8 @@
 
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
+import { Icon } from 'react-native-elements'
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import RestaurantsStack from '../navigations/RestaurantsStack'
 import AccountStack from '../navigations/AccountStack'
@@ -13,9 +15,53 @@ import TopRestaurantsStack from '../navigations/TopRestaurantsStack'
 const Tab =  createBottomTabNavigator()
 
 export default function Navigation() {
+
+    const screenOptions = (route, color) => {
+        let iconName 
+        switch (route.name) {
+            case "restaurants":
+                iconName = "silverware-fork-knife"                
+                break;
+            case "favorites":
+                iconName = "star-outline"                
+                break;
+            case "account":
+                iconName = "account-circle-outline"                
+                break;
+            case "search":
+                iconName = "selection-search"                
+                break;                    
+            case "topRestaurants":
+                iconName = "arrow-top-right-bold-outline"               
+                break;                    
+    
+            default:
+                break;
+        }
+
+        return (
+            <Icon
+               type="material-community"
+               name = {iconName}
+               size = {22}
+               color = {color}
+            />
+        )
+    }
+
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                initialRouteName = "restaurants"
+                tabBarOptions ={{
+                    inactiveTintColor : "#e0d8c8",
+                    activeTintColor : "#050505"
+                        
+                }}
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({color}) => screenOptions(route, color)
+                })}>                            
+                
                 <Tab.Screen
                     name="restaurants"
                     component ={RestaurantsStack}
