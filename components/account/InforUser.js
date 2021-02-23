1fc6f0b232846e524b9fa018c0edbb6e8edcfb51
Joel2508 <Joelarias2508@gmail.com>
2021-02-23 15:37:54 -0400

    Add inforuser.js and navigator is the view

---
+++ components/account/InforUser.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function InforUser({ user }) {
+    return (
+        <View>
+            <Text>InforUser</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
--- screens/account/UserLogged.js
+++ screens/account/UserLogged.js
@@ -1,23 +1,66 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Button } from 'react-native-elements'
 import {useNavigation} from '@react-navigation/native'
-import { closeSession } from '../../util/action'
+import { closeSession, getCurrentUser } from '../../util/action'
+ 
+import Toast from  'react-native-easy-toast'
+import InforUser from '../../components/account/InforUser'
+import Loading from '../../components/Loading'
+
 
 export default function UserLogged() {
 
+    const toastRef = useRef()
     const navigation = useNavigation()
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [loadingText, setLoadingText] = useState("")
+
+    useEffect(() => {
+
+        setUser(getCurrentUser)
+
+    }, [])
     return (
-        <View>
-            <Text>UserLogged....</Text>
+        <View style={styles.container}>
+             {
+               user &&   <InforUser user = {user}/>             
+             }
+            <Text>Account User....</Text>
             <Button 
+            buttonStyle = {styles.btnLogOut}
+            titleStyle = {styles.btnLogOutTitle}
             title="Log Out"
             onPress ={() =>{ 
                 closeSession()
                 navigation.navigate("restaurants")
                 }}/>
+                <Toast ref={toastRef} position="center" opacity={0.9}/>
+                <Loading isVisible ={loading} text ={loadingText}/>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+    container: {
+        minHeight : "100%",
+        
+    },
+    btnLogOut : {
+      marginTop : 30,
+      margin : 30,
+      borderRadius : 20,
+      backgroundColor: "#3c3c4c",
+      borderTopWidth : 1,
+      borderTopColor : "#ffffff",
+      borderBottomWidth :1,
+      borderBottomColor : "#c1c1c1",
+      paddingVertical : 10,
+    }, 
+    btnLogOutTitle : {
+        color: "#c1c1c1"
+    }
+
+})
