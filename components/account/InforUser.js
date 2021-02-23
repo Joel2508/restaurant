1a57560519bae4b77091e671d7742905f96ba9fa
Joel2508 <Joelarias2508@gmail.com>
2021-02-23 16:19:16 -0400

    Modification inforuser.js add avatar and get information the user

--- components/account/InforUser.js
+++ components/account/InforUser.js
@@ -1,12 +1,52 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Avatar } from 'react-native-elements'
 
 export default function InforUser({ user }) {
+    console.log(user)
     return (
-        <View>
-            <Text>InforUser</Text>
+        <View style={styles.container}>
+            <Avatar 
+             rounded
+             size ="large"
+             containerStyle ={styles.avatar}
+             source ={
+                 user.photoURL ? {uri : photoURL} : require("../../assets/avatar1.png")
+             }
+            />
+            <View style={styles.inforUser}>
+                <Text style={styles.displayName}>
+                    {
+                        user.displayName ? user.displayName : "Anonymous"
+                    }
+                </Text>
+                <Text>
+                    {user.email}
+                </Text>
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        alignItems : 'center',
+        justifyContent : 'center',
+        flexDirection : "row",
+        backgroundColor : "#f9f9f9",
+        paddingVertical : 30
+    },
+    avatar : {
+        borderColor : "#c1c1c1",
+        backgroundColor : "#ffffff",
+        width : 100,
+        height: 100
+    },
+    inforUser: {
+     marginLeft : 20,     
+    },
+    displayName :{
+      fontWeight : "bold",
+      paddingBottom : 5
+    }
+})
