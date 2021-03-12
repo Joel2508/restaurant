01f49224797b322a98e89300f53b79924f1005b6
Joel2508 <Joelarias2508@gmail.com>
2021-03-12 16:29:44 -0400

    Add new componet mapRestaurant

---
+++ components/restaurants/MapRestaurant.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
+
+import {openMap} from 'react-native-open-maps'
+export default function MapRestaurant({location, name, height}) {
+    const openAppMap =() => {
+      openMap({
+          latitude: location.latitude,
+          longitude : location.longitude,
+          zoom: 19,
+          query : name
+      })
+    }
+    return (
+        <MapView style= {{height: height, witdh : "100%"}} initialRegion = {location}
+        onPress = {openAppMap}>
+
+        <Marker
+        coordinate ={{
+        latitude : location.latitude,
+         longitude : location.longitude}}
+         />
+
+        </MapView>
+    )
+}
+
+const styles = StyleSheet.create({
+    mapView: {
+        height :100
+    }
+})
--- screens/restaurants/Restaurant.js
+++ screens/restaurants/Restaurant.js
@@ -1,11 +1,16 @@
 import React, {useState, useEffect} from 'react'
 import { Alert, Dimensions, ScrollView } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
-import { Rating } from 'react-native-elements'
+import { Icon, ListItem, Rating } from 'react-native-elements'
+import MapView from 'react-native-maps'
 
+
+import {map} from 'loadsh'
 import CarouselImage from '../../components/CarouselImage'
 import Loading from '../../components/Loading'
+import MapRestaurant from '../../components/restaurants/MapRestaurant'
 import { getDocumentById } from '../../util/action'
+import { formatPhone } from '../../util/helper'
 
 const widthDimension = Dimensions.get("window").width
 
@@ -21,7 +26,6 @@ export default function Restaurant({navigation, route}) {
 
         (async ()=>{
             const response = await getDocumentById("restaurants", id)
-            console.log(response)
             if(response.statusResponse){
                setRestaurant(response.document)
             }else{
@@ -47,9 +51,53 @@ export default function Restaurant({navigation, route}) {
                 name = {restaurant.name}
                 description= {restaurant.description}
                 rating = {restaurant.rating}/>
+            <RestaurantInfo
+                name={restaurant.name}
+                location ={restaurant.location}
+                address = {restaurant.address}
+                email ={restaurant.email}
+                phone = {formatPhone(restaurant.callingCode, restaurant.phone)}
+                />
         </ScrollView>
     )
 }
+
+function RestaurantInfo({name, location, address, email, phone}) {
+    const listInfo = [
+        {text: address, iconName: "map-marker-outline"},
+        {text: email, iconName: "at"},        
+        {text: phone, iconName: "phone"},
+    ]
+
+    return (
+        <View style ={styles.viewRestaurantInfo}>
+            <Text style ={styles.infoTitleRestaurant}>
+                Information the restaurant
+            </Text>
+          <MapRestaurant 
+            location ={location}
+            name ={name}
+            height={150}/>
+            {
+                map(listInfo, (item, index) => (
+                    <ListItem
+                        key ={index} 
+                        style = {styles.containerList}>                        
+                        <Icon
+                            type ="material-community"
+                            name = {item.iconName}
+                            color ="#3c3c4c"/>                        
+                        <ListItem.Content>
+                            <ListItem.Title>{item.text}</ListItem.Title>
+                        </ListItem.Content>
+                    </ListItem>                    
+                ))            
+            }
+        </View>
+    )
+}
+
+
 
 function TitleRestaurant ({name, description, rating}) {
    return (
@@ -66,6 +114,7 @@ function TitleRestaurant ({name, description, rating}) {
        </View>
    )
 }
+
 
 const styles = StyleSheet.create({
     viewScroll : {
@@ -89,5 +138,18 @@ const styles = StyleSheet.create({
     },
     nameRestaurant : {
         fontWeight: "bold"
+    },
+    viewRestaurantInfo: {
+        margin : 15,
+        marginTop : 25        
+    },
+    infoTitleRestaurant : {
+        fontSize : 18,
+        fontWeight: "bold",
+        marginBottom : 5    
+    },
+    containerList : {
+       borderBottomColor : "#7c7c7c",
+       borderBottomWidth : 1 
     }
 })
